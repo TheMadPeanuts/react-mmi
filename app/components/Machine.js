f548f7fc71a0260f90a021fc74e0f8dbbdb4ae34
Contente Dinis <diniscontente@hotmail.fr>
2018-03-26 06:58:25 +0000

    Création du composant Machine

--- app/components/Machine.js
+++ app/components/Machine.js
@@ -8,7 +8,21 @@ import React from 'react';
 class Machine extends React.Component {
   render() {
     // Dans tous les cas, afficher
-    return <p>Machine 1</p>
+    return (
+      <div className="Machine">
+  
+      <div className="MachineHeader">
+        <h3>{this.props.title} | {this.props.isActive}</h3>
+        
+      </div>
+      
+      <div className="Machine1">
+        <p>je dis {this.props.title} | {this.props.isActive}</p>
+      </div>
+      
+      </div>
+      
+      );
   }
 }
 
--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -7,13 +7,20 @@ import React from 'react';
 
 // On importe ici les composants qu'on veut afficher
 import Machine from '../../components/Machine.js';
+import header from '../../components/Header.js';
+import footer from '../../components/Footer.js';
 // On peut aussi importer du CSS de la meme facon.
+import css from '../../containers/CSS/Style.css';
 
 export default function App() {
   return (
     // Un return doit retourner un seul élément du DOM
     // Si on veut afficher plusieurs éléments adjacents,
     // On devra donc les encapsuler dans une DIV parente.
-    <Machine />
+    <div>
+      <Machine title="Shoking !" isActive ="yes"/>
+      <Machine title="Secouons-nous les mains" isActive ="Of course !"/>
+      <Machine title="Je demande votre pardon ?" isActive ="Well"/>
+    </div>
   );
 }
